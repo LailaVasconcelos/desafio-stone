93d957526acfaa2c1db764a56f649ec609301d75
Laila Vasconcelos <lailavasconcelos814@gmail.com>
2020-08-19 21:47:28 -0300

    Códigos das moedas alterados de string para atom. Alteração feita nos testes também.

--- lib/desafio_stone.ex
+++ lib/desafio_stone.ex
@@ -3,14 +3,14 @@ defmodule DesafioStone do
   Documentation para DesafioStone.
   """
 
-  @conversoes %{"BRL" => %{"EUR" => 0.22, "USD" => 0.17}, "EUR" => %{"BRL" => 6.3}}
+  @conversoes %{:BRL => %{:EUR => 0.22, :USD => 0.17}, :EUR => %{:BRL => 6.3}}
 
   @doc """
   Faz a conversão de valores entre moedas
 
   ## Examples
 
-      iex> DesafioStone.converte_moeda("BRL", "EUR", 100)
+      iex> DesafioStone.converte_moeda(:BRL, :EUR, 100)
       22.0 # Valor convertido de reais brasileiros (BRL) para euros (EUR)
       
 
--- test/desafio_stone_test.exs
+++ test/desafio_stone_test.exs
@@ -3,9 +3,9 @@ defmodule DesafioStoneTest do
   doctest DesafioStone
 
   test "Verifica se a conversão de duas moedas conhecidas está correta" do
-    assert DesafioStone.converte_moeda("BRL", "EUR", 55) == 12.1
+    assert DesafioStone.converte_moeda(:BRL, :EUR, 55) == 12.1
 
   test "Verifica se retorna nulo para conversão não suportadas" do
-    assert DesafioStone.converte_moeda("BRL", "ARS", 55) == nil  
+    assert DesafioStone.converte_moeda(:BRL, :ARS, 55) == nil  
   end
 end

735bab5a5c9584085980ee167c0320cc707d4a8a
LailaVasconcelos <lailavasconcelos8114@gmail.com>
2020-08-28 16:41:25 -0300

    Criada a função split_teste

---
+++ test/split_test.exs
@@ -0,0 +1,32 @@
+defmodule SplitTest do
+  use ExUnit.Case
+  doctest Split
+
+  test "Testa os valores do resultado do split para 3 pessoas" do
+    lista_pessoas_split = [
+      %{:nome => "Maria", :peso => Decimal.new(5)},
+      %{:nome => "Joaquim", :peso => Decimal.new(3)},
+      %{:nome => "Marina", :peso => Decima.new(10)},
+    ]
+    resultado_esperado = [
+      %{:nome => "Maria", :peso => Decimal.new(5), :valor => Decimal.new("138.89")},
+      %{:nome => "Joaquim", :peso => Decimal.new(3), :valor => Decimal.new("83.33")},
+      %{:nome => "Marina", :peso => Decima.new(10), :valor => Decimal.new("277.78")},
+    ]
+
+    assert Split.operacao_split(lista_pessoas_splt, Decimal.new("500")) == resultado_esperado
+  end
+
+  test "Verifica se o split está com problemas de arrendodamento" do
+    valor_original = Decimal.new("500.00")
+    lista_pessoas_split = [
+      %{:nome => "Maria", :peso => Decimal.new(5)},
+      %{:nome => "Joaquim", :peso => Decimal.new(3)},
+      %{:nome => "Marina", :peso => Decima.new(10)},
+    ]
+    resulatado = Split.operacao_split(lista_pessoas_split, valor_original)
+    valor_total = resultado |> Enum.map(fn item -> item[:valor] end) |> Enum.reduce(Decimal.new("0"), fn (a, b) -> Decimal.add(a, b) end)
+
+    assert valor_total == valor_original
+  end
+end

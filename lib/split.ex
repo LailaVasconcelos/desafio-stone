e7c5052cdd49720919576e7669b2e4b9c2d42f3c
LailaVasconcelos <lailavasconcelos8114@gmail.com>
2020-08-29 19:45:28 -0300

    Criada a função operacao_split.

---
+++ lib/split.ex
@@ -0,0 +1,17 @@
+defmodule Split do
+  @moduledoc """
+  Documentation para Split.
+  """
+
+  def operacao_split(lista_pessoas, valor) do
+    alias Decimal, as: D
+
+    total_pesos = Enum.map(lista_pessoas, fn item -> item[:peso] end) 
+    |> Enum.reduce(D.new("0"), fn (a, b) -> D.add(a, b) end)
+    
+    divisao = D.div(valor, total_pesos)
+
+    item_valor_inserido = Enum.map(lista_pessoas, fn item -> Map.put(item, :valor, D.mult(divisao, item[:peso])) end)     
+    
+  end
+end
--- test/split_test.exs
+++ test/split_test.exs
@@ -6,15 +6,15 @@ defmodule SplitTest do
     lista_pessoas_split = [
       %{:nome => "Maria", :peso => Decimal.new(5)},
       %{:nome => "Joaquim", :peso => Decimal.new(3)},
-      %{:nome => "Marina", :peso => Decima.new(10)},
+      %{:nome => "Marina", :peso => Decimal.new(10)},
     ]
     resultado_esperado = [
       %{:nome => "Maria", :peso => Decimal.new(5), :valor => Decimal.new("138.89")},
       %{:nome => "Joaquim", :peso => Decimal.new(3), :valor => Decimal.new("83.33")},
-      %{:nome => "Marina", :peso => Decima.new(10), :valor => Decimal.new("277.78")},
+      %{:nome => "Marina", :peso => Decimal.new(10), :valor => Decimal.new("277.78")},
     ]
 
-    assert Split.operacao_split(lista_pessoas_splt, Decimal.new("500")) == resultado_esperado
+    assert Split.operacao_split(lista_pessoas_split, Decimal.new("500")) == resultado_esperado
   end
 
   test "Verifica se o split está com problemas de arrendodamento" do
@@ -22,9 +22,9 @@ defmodule SplitTest do
     lista_pessoas_split = [
       %{:nome => "Maria", :peso => Decimal.new(5)},
       %{:nome => "Joaquim", :peso => Decimal.new(3)},
-      %{:nome => "Marina", :peso => Decima.new(10)},
+      %{:nome => "Marina", :peso => Decimal.new(10)},
     ]
-    resulatado = Split.operacao_split(lista_pessoas_split, valor_original)
+    resultado = Split.operacao_split(lista_pessoas_split, valor_original)
     valor_total = resultado |> Enum.map(fn item -> item[:valor] end) |> Enum.reduce(Decimal.new("0"), fn (a, b) -> Decimal.add(a, b) end)
 
     assert valor_total == valor_original
